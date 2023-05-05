7a604cf57f5fe8099695dabeb49077613f4103fc
김윤욱 <reznov03048@gmail.com>
2023-05-05 14:19:30 +0900

    feat: 내부 메소드 호출 시 트랜잭션 작용 안되는 경우

---
+++ src/test/java/hello/springtx/apply/InternalCallV1Test.java
@@ -0,0 +1,64 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class InternalCallV1Test {
+
+    @Autowired
+    CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        callService.internal();
+    }
+
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    @Slf4j
+    static class CallService {
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internal(); // 밑 처럼 this 자동으로 생략됨
+//            this.internal(); // -> 프록시를 거치지 않고 원본의 메소드를 호출하게 됨
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
